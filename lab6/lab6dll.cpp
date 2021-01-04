76f06defe5651be0be1e5da4327a5bab322332c1
Ivan <72566481+IvanRud10@users.noreply.github.com>
2021-01-04 17:48:49 +0200

    Update lab6dll.cpp

--- lab6/lab6dll.cpp
+++ lab6/lab6dll.cpp
@@ -3,15 +3,13 @@
 #include "Header.h"
 using namespace std;
 extern "C" __declspec(dllexport) int sumOfNumbers(char* a, char let) {
-	//char a[512];
 	int sum = 0;
-	//char let;
 	for (int i = 0; i < strlen(a); ++i)
 	{
-			if (a[i] == let)
-			{
-				sum += i;
-			}
+		if (a[i] == let)
+		{
+			sum += i;
+		}
 	}
 	return sum;
 }
